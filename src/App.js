38066f85fad5020f9078b1fc979c652f95e08dbf
{vongocchinh} <{ngocchinh1410@gmail.com}>
2021-01-18 08:44:13 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import  { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import parser from 'html-react-parser'
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      data:localStorage.getItem("demo")
+    }
+  }
+  onchange=(e,editor)=>{
+    var data=editor.getData();
+    this.setState({
+      data
+    });
+  }
+  render() {
+    console.log(this.state.data);
+    return (
+      <>
+        <CKEditor
+        data={this.state.data}
+        editor={ClassicEditor}
+        onReady={editor=>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        }}
+        onChange={this.onchange}
+        style={{height:"500px"}}
+         />
+
+         <button onClick={this.onSave}>save</button>
+         <div>
+           {parser(this.state.data)}
+         </div>
+      </>
+    )
+  }
+  onSave=()=>{
+    console.log(this.state.data);
+    localStorage.setItem("demo",(this.state.data));
+  }
 }
-
-export default App;
